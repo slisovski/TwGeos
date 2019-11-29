8facef44d6037914f41d01bed240583979947688
Lisovski Simeon <Simeon.Lisovski@vogelwarte.ch>
2019-11-29 12:00:06 +0100

    fix thresholdCalib

--- R/TwGeos.R
+++ R/TwGeos.R
@@ -635,69 +635,79 @@ findTwilights <- function(tagdata,threshold,include,
 ##' @export
 thresholdCalibration <- function(twilight, rise, lon, lat, method = "log-normal", plot= TRUE) {
  
-  if(!(method%in%c("gamma", "log-norm"))) stop("Method can only be `gamma` or `log-norm`.")
-  
+  if (!(method %in% c("gamma", "log-norm"))) 
+    stop("Method can only be `gamma` or `log-norm`.")
   tab <- data.frame(Twilight = twilight, Rise = rise)
-  
-  sun  <- solar(tab[,1])
-  z    <- refracted(zenith(sun, lon, lat))
-  # plot(z)
-  
+  sun <- solar(tab[, 1])
+  z <- refracted(zenith(sun, lon, lat))
   inc = 0
   repeat {
-    twl_t   <- twilight(tab[,1], lon, lat, rise = tab[,2], zenith = max(z)+inc)
-    twl_dev <- ifelse(tab$Rise, as.numeric(difftime(tab[,1], twl_t, units = "mins")),
-                      as.numeric(difftime(twl_t, tab[,1], units = "mins")))
-    if(all(twl_dev>=0)) {
+    twl_t <- SGAT::twilight(tab[, 1], lon, lat, rise = tab[, 2], zenith = max(z) + inc, iters = 5, closest = TRUE)
+    
+    twl_dev <- ifelse(tab$Rise, as.numeric(difftime(tab[, 1], twl_t, units = "mins")), 
+                      as.numeric(difftime(twl_t, tab[, 1], units = "mins")))
+    if (all(twl_dev >= 0) | inc>1.5) {
       break
-    } else {
-      inc <- inc+0.01
+    }
+    else {
+      inc <- inc + 0.01
     }
   }
-  z0 <- max(z)+inc
   
-  seq <- seq(0, max(twl_dev), length = 100)
+  OutS <- rep(TRUE, length(twl_dev))
+  if(any(abs(twl_dev)>12*60)) OutS[abs(twl_dev)>12*60] <- FALSE
+  twl_dev[!OutS] <- NA
+  twl_t[!OutS]   <- NA
   
-  if(method=="log-norm"){
-    fitml_ng <- suppressWarnings(fitdistr(twl_dev, "log-normal"))
-    lns      <- dlnorm(seq, fitml_ng$estimate[1], fitml_ng$estimate[2])
+  z0 <- max(z) + inc
+  
+  seq <- seq(0, max(twl_dev, na.rm = T), length = 100)
+  if (method == "log-norm") {
+    fitml_ng <- suppressWarnings(fitdistr(twl_dev[!is.na(twl_dev)], "log-normal"))
+    lns <- dlnorm(seq, fitml_ng$estimate[1], fitml_ng$estimate[2])
   }
-  if(method=="gamma"){
-    fitml_ng <- suppressWarnings(fitdistr(twl_dev, "gamma"))
-    lns      <- dgamma(seq, fitml_ng$estimate[1], fitml_ng$estimate[2])
+  if (method == "gamma") {
+    fitml_ng <- suppressWarnings(fitdistr(twl_dev[!is.na(twl_dev)], "gamma"))
+    lns <- dgamma(seq, fitml_ng$estimate[1], fitml_ng$estimate[2])
   }
+  diffz <- as.data.frame(cbind(min = apply(cbind(tab[, 1], twl_t), 1, 
+                                           function(x) abs(x[1] - x[2]))/60, z = z))
   
-  
-  diffz <- as.data.frame(cbind(min = apply(cbind(tab[,1], twilight(tab[,1], lon,lat, rise = tab[,2], zenith = z0)), 1, function(x) abs(x[1]-x[2]))/60, z = z))
-  mod  <- lm(z~min, data = diffz)
-  mod2 <- lm(min~z, data = diffz)
-  
+  mod <-  lm(z ~ min,  data = diffz)
+  mod2 <- lm(min ~ z, data = diffz)
   z1 <- median(z)
-  
-  if(plot) {
+  if (plot) {
     opar <- par(mar = c(10, 4, 1, 1))
-    if(method=="log-norm") hist(twl_dev, freq = F, breaks = 26, main = "Twilight Model (log-norm)", xlab = "twilight error (min)")
-    if(method=="gamma") hist(twl_dev, freq = F, breaks = 26, main = "Twilight Model (gamma)", xlab = "twilight error (min)")
+    if (method == "log-norm") 
+      hist(twl_dev, freq = F, breaks = 26, main = "Twilight Model (log-norm)", 
+           xlab = "twilight error (min)")
+    if (method == "gamma") 
+      hist(twl_dev, freq = F, breaks = 26, main = "Twilight Model (gamma)", 
+           xlab = "twilight error (min)")
     lines(seq, lns, col = "firebrick", lwd = 3, lty = 2)
-    
-    points(predict(mod2, newdata=data.frame(z = z0)), 0, pch = 21, cex = 5, bg = "white", lwd = 2)
-    text(predict(mod2, newdata=data.frame(z = z0)), 0, "0")
-    
-    points(z1, 0, pch = 21, cex = 5, bg = "white", lwd = 2)
-    text(z1, 0, "1")
-    
-    axis(1, at = seq(0, max(twl_dev), 6), labels = round(90-predict(mod, newdata = data.frame(min = seq(0, max(twl_dev), 6))),1), line = 5)
+    points(predict(mod2, newdata = data.frame(z = z0)), 0, 
+           pch = 21, cex = 5, bg = "white", lwd = 2)
+    text(predict(mod2, newdata = data.frame(z = z0)), 0, 
+         "0")
+    points(predict(mod2, data.frame(z = z1)), 0, pch = 21, cex = 5, bg = "white", lwd = 2)
+    text(predict(mod2, data.frame(z = z1)), 0, "1")
+    axis(1, at = seq(0, max(twl_dev, na.rm = T), 6), labels = round(90 - 
+                                                                      predict(mod, newdata = data.frame(min = seq(0, max(twl_dev, na.rm = T), 
+                                                                                                                  6))), 1), line = 5)
     mtext("sun elevation angle (degrees)", 1, line = 8)
-    
-    if(method=="log-norm") legend("topright", paste(c("0. Zenith angle (zero)", "1. Zenith angle (median)", "log-mean", "log-sd"), 
-                                                    round(c(z0, z1, fitml_ng$estimate[1], fitml_ng$estimate[2]),3)), bty = "n")
-    if(method=="gamma") legend("topright", paste(c("0. Zenith angle (zero)", "1. Zenith angle (median)", "shape", "scale"), 
-                                                    round(c(z0, z1, fitml_ng$estimate[1], fitml_ng$estimate[2]),3)), bty = "n")
-    
+    if (method == "log-norm") 
+      legend("topright", paste(c("0. Zenith angle (zero)", 
+                                 "1. Zenith angle (median)", "log-mean", "log-sd"), 
+                               round(c(z0, z1, fitml_ng$estimate[1], fitml_ng$estimate[2]), 
+                                     3)), bty = "n")
+    if (method == "gamma") 
+      legend("topright", paste(c("0. Zenith angle (zero)", 
+                                 "1. Zenith angle (median)", "shape", "scale"), 
+                               round(c(z0, z1, fitml_ng$estimate[1], fitml_ng$estimate[2]), 
+                                     3)), bty = "n")
     par(opar)
   }
-  
-  c(a1 = z1, e0 = z0, log.mean =  fitml_ng$estimate[1], log.sd =  fitml_ng$estimate[2])
+  c(z1 = z1, z0 = z0, log.mean = fitml_ng$estimate[1], log.sd = fitml_ng$estimate[2])
 }
 
 
